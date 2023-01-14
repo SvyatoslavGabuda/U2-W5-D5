6425002916c9fe8a789f61278fe320f2dcb98324
SvyatoslavGabuda <sgabuda@gmail.com>
2023-01-14 11:17:14 +0100

    mmm

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,26 @@ const cambioColore = function () {
   }
 };
 window.addEventListener("scroll", cambioColore);
+
+const letteraM = document.querySelectorAll(`svg g[stroke-linecap="butt" ]`);
+console.log("gruppo g", letteraM);
+let numero = 0;
+
+const tempo = setInterval(() => {
+  const random = Math.floor(Math.random() * letteraM.length);
+  const randomM = letteraM[random];
+  const randomMAttribut = randomM.getAttribute("opacity");
+  // console.log(random);
+  // console.log(randomM);
+  // console.log("opacity value: " + randomMAttribut);
+  numero++;
+
+  if (randomMAttribut == 0) {
+    randomM.setAttribute("opacity", "1");
+  } else {
+    randomM.setAttribute("opacity", "0");
+  }
+  // if (numero === 1000) {
+  //   clearInterval(tempo);
+  // }
+}, 100);
